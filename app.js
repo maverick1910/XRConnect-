4ec4cfcd4e87d633167e4ab26b73b0700f18acd6
Akhil Konduru <60151778+maverick1910@users.noreply.github.com>
2020-12-06 14:10:59 +0530

    Routes Set Up

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+var PORT= process.env.PORT || 3000
 var express =require('express');
 var mysql=require('mysql');
 const app=express();
+var bodyparser=require("body-parser")
+
+
+
+//APP CONFIG
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+app.use('*/css', express.static('public/css'));
+app.use('*/js', express.static('public/js'));
+app.use('*/img', express.static('public/img'));
+app.use('*/fonts', express.static('public/fonts'));
+app.use('*/vendors', express.static('public/vendors'));
 
 var conn=mysql.createConnection({
     host:'localhost',
@@ -25,4 +41,19 @@ app.get('/rows', function (req, res) {
   });
 });
 
-app.listen(3000,console.log('server up'));
+app.get('/',function(req,res){
+  res.render('index');
+
+});
+app.get('/register',function(req,res){
+  res.render('register');
+
+});
+app.post('/admin',function(req,res){
+  res.render('admin')
+})
+app.get('/join',function(req,res){
+  res.render('join')
+})
+
+app.listen(PORT,console.log('server up'));
